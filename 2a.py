3a512ff9e0803e377d2db2c4438c5176c68c69d7
vinay55a <avinay382@gmail.com>
2024-09-18 12:58:57 +0530

    Add files via upload

---
+++ 2a.py
@@ -0,0 +1,12 @@
+def fib(n):
+    if(n==0 or n==1):
+        return n
+    else:
+        return(fib(n-1)+fib(n-2))
+num=int(input("Enter a number"))
+if(num>=0):
+    f=fib(num)
+    print("fib sequence is",f)
+else:
+    print("Not a valid number")
+
