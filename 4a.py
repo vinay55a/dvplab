e1410d39cc0f019fa09c4c5b2b18e8b733aaa3a2
vinay55a <avinay382@gmail.com>
2024-10-09 13:09:35 +0530

    Add files via upload

---
+++ 4a.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+std=['sslc','puc','1sem','2sem']
+values=[500,600,680,690]
+plt.bar(std,values,color='black')
+plt.xlabel("Education",fontsize=12)
+plt.ylabel("Marks obtained",fontsize=12)
+plt.title("Vikas-1KI23CS180")
+plt.show()
---
+++ 4b.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[-1,0,1,2,3,]
+plt.scatter(x,y,color='black')
+plt.xlabel("xaxis",fontsize=12)
+plt.ylabel("yaxis",fontsize=12)
+plt.title("Vikas-1KI23CS180")
+plt.show()
