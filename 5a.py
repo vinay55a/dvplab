996bdcee4521984a87d3a6d0826a53603a19c783
vinay55a <avinay382@gmail.com>
2024-10-22 23:22:43 -0700

    Add files via upload

---
+++ 5a.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.hist([6,8,12,16,10,20,13,15,17,7,5,9],bins=4)
+plt.xlabel("Distribution of age",fontsize=16)
+plt.ylabel("Frequency",fontsize=16)
+plt.title("Vinay-1KI23CS182")
+plt.show()
---
+++ 5b.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+CARS=['KIA','BMW','BENZ','TATA','AUDI','polo']
+Data=[40,20,18,15,10,9]
+plt.figure(figsize=(10,7))
+plt.pie(Data,labels=CARS)
+plt.title("Vinay A-1KI23CS182")
+plt.show()
